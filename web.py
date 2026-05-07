ffb0321a08232126a723a0e4bed6e4d760940750
elephants950122-coder <elephants950122@gmail.com>
2026-05-07 10:56:02 +0800

    opendata1

--- web.py
+++ web.py
@@ -37,7 +37,21 @@ def index():
     link += "<a href='/movie1'>爬取即將上映電影</a><hr>"
     link += "<a href='/spiderMovie'>爬取即將上映電影並存入資料庫</a><hr>"
     link += "<a href='/searchMovie'>從資料庫搜尋電影</a><hr>"
+    link += "<a href='/road'>台中市十大肇事路口</a><hr>"
     return link
+
+@app.route("/road")
+def road():
+    R = "<h1>台中市十大肇事路口(113年10月)</h1><br>"
+    url = "https://datacenter.taichung.gov.tw/swagger/OpenData/a1b899c0-511f-4e3d-b22b-814982a97e41"
+    Data = requests.get(url)
+    #print(Data.text)
+
+    JsonData = json.loads(Data.text)
+    for item in JsonData:
+        R += item["路口名稱"] + ", 原因:" + item["主要肇因"] + "<br>"
+    
+    return R
 
 @app.route("/searchMovie")
 def searchMovie():
